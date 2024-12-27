5b441e9ae272c9a0355c2eb27f9f00d088a8ff24
Andy-spb82 <Antikaspb2310@gmail.com>
2024-12-27 19:14:00 +0300

    Add files via upload

---
+++ инд. строк.py
@@ -0,0 +1,18 @@
+
+students={('Johnny'),('Bilbo'),('Steve'),('Khendrik'),('Aaron')}
+b = (students)
+b = sorted(b)
+b = tuple(b)
+print (b)
+grades=[(5,3,3,5,4),(2,2,2,3),(4,5,5,2),(4,4,3),(5,5,5,4,5)]
+a=grades
+for i in range (len(a)):
+ a[i] = sum(a[i])/len (a[i])
+print (a)
+EEE_book= dict.fromkeys (b,a[i])
+print (EEE_book)
+
+
+
+
+
